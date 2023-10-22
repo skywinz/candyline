b51bf7c92dc29592443670ee446e8a0c8deb5def
recoma <seokbong60@gmail.com>
2023-10-22 11:06:17 +0900

    fix: print right month

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,5 @@
 export const date2String = (date: Date, isTimeExtended: boolean = false): string => {
-    let s = `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일`;
+    let s = `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
 
     if (isTimeExtended) {
         s += ` ${date.getHours()}시 ${date.getMinutes()}분 ${date.getSeconds()}초`;
